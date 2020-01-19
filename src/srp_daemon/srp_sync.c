d2475b6950278bfc1738e478f57bb5ef444d3f7d
Alex Baranowski <alex@euro-linux.com>
2020-01-19 14:31:27 +0100

    els-ASAP auto commit file/tag srptools-0.0.4-17.el7.src.rpm

---
+++ src/srp_daemon/srp_sync.c
@@ -0,0 +1,221 @@
+/*
+ * srp_sync - discover SRP targets over IB
+ * Copyright (c) 2005 Topspin Communications.  All rights reserved.
+ * Copyright (c) 2006 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2006 Mellanox Technologies Ltd.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * $Author: ishai Rabinovitz [ishai@mellanox.co.il]$
+ */
+
+#include <pthread.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "srp_daemon.h"
+
+int sync_resources_init(struct sync_resources *res)
+{
+	int ret;
+
+	res->stop_threads = 0;
+	res->recalc = 1;
+	res->next_task = 0;
+	ret = pthread_mutex_init(&res->mutex, NULL);
+	if (ret < 0) {
+		pr_err("coult not initilize mutex\n");
+		return ret;
+	}
+	ret = pthread_cond_init(&res->cond, NULL);
+	if (ret < 0)
+		pr_err("coult not initilize cond\n");
+
+	res->retry_tasks_head = NULL;
+	ret = pthread_mutex_init(&res->retry_mutex, NULL);
+	if (ret < 0) {
+		pr_err("coult not initilize mutex\n");
+		return ret;
+	}
+	ret = pthread_cond_init(&res->retry_cond, NULL);
+	if (ret < 0)
+		pr_err("coult not initilize cond\n");
+
+	return ret;
+}
+
+void push_gid_to_list(struct sync_resources *res, ib_gid_t *gid)
+{
+	int i;
+
+	/* If there is going to be a recalc soon - do nothing */
+	if (res->recalc)
+		return;
+
+	pthread_mutex_lock(&res->mutex);
+
+	/* check if the gid is already in the list */
+
+	for (i=0; i < res->next_task; ++i)
+		if (!memcmp(&res->tasks[i].gid, gid, 16)) {
+			pr_debug("gid is already in task list\n");
+			pthread_mutex_unlock(&res->mutex);
+			return;
+		}
+
+	if (res->next_task == SIZE_OF_TASKS_LIST) {
+		/* if the list is full, lets do a full rescan */
+
+		res->recalc = 1;
+		res->next_task = 0;
+	} else {
+		/* otherwise enter to the next entry */
+
+		res->tasks[res->next_task].gid = *gid;
+		res->tasks[res->next_task].lid = 0;
+		++res->next_task;
+	}
+
+	pthread_cond_signal(&res->cond);
+	pthread_mutex_unlock(&res->mutex);
+}
+
+void push_lid_to_list(struct sync_resources *res, uint16_t lid)
+{
+	int i;
+
+	/* If there is going to be a recalc soon - do nothing */
+	if (res->recalc)
+		return;
+
+	pthread_mutex_lock(&res->mutex);
+
+
+	/* check if the lid is already in the list */
+
+	for (i=0; i < res->next_task; ++i)
+		if (res->tasks[i].lid == lid) {
+			pr_debug("lid %d is already in task list\n", lid);
+			pthread_mutex_unlock(&res->mutex);
+			return;
+		}
+
+	if (res->next_task == SIZE_OF_TASKS_LIST) {
+		/* if the list is full, lets do a full rescan */
+
+		res->recalc = 1;
+		res->next_task = 0;
+	} else {
+		/* otherwise enter to the next entry */
+
+		res->tasks[res->next_task].lid = lid;
+		memset(&res->tasks[res->next_task].gid, 0, 16);
+		++res->next_task;
+	}
+
+	pthread_cond_signal(&res->cond);
+	pthread_mutex_unlock(&res->mutex);
+}
+
+void clear_traps_list(struct sync_resources *res)
+{
+	pthread_mutex_lock(&res->mutex);
+	res->next_task = 0;
+	pthread_mutex_unlock(&res->mutex);
+}
+
+
+/* assumes that res->mutex is locked !!! */
+int pop_from_list(struct sync_resources *res, uint16_t *lid, ib_gid_t *gid)
+{
+	int ret=0;
+	int i;
+
+	if (res->next_task) {
+		*lid = res->tasks[0].lid;
+		*gid = res->tasks[0].gid;
+		/* push the rest down */
+		for (i=1; i < res->next_task; ++i)
+			res->tasks[i-1] = res->tasks[i];
+		ret = 1;
+		--res->next_task;
+	}
+
+	return ret;
+}
+
+
+/* assumes that res->retry_mutex is locked !!! */
+struct target_details *pop_from_retry_list(struct sync_resources *res) 
+{
+	struct target_details *ret = res->retry_tasks_head;
+
+	if (ret)
+		res->retry_tasks_head = ret->next;
+	else
+		res->retry_tasks_tail = NULL;
+
+	return ret;
+}
+
+void push_to_retry_list(struct sync_resources *res,
+			struct target_details *orig_target)
+{
+       struct target_details *target;
+
+	/* If there is going to be a recalc soon - do nothing */
+	if (res->recalc)
+		return;
+
+	target = malloc(sizeof(struct target_details));
+	memcpy(target, orig_target, sizeof(struct target_details));
+
+	pthread_mutex_lock(&res->retry_mutex);
+
+	if (!res->retry_tasks_head)
+		res->retry_tasks_head = target;
+
+	if (res->retry_tasks_tail)
+		res->retry_tasks_tail->next = target;
+
+	res->retry_tasks_tail = target;
+
+	target->next = NULL;
+
+	pthread_cond_signal(&res->retry_cond);
+	pthread_mutex_unlock(&res->retry_mutex);
+}
+
+/* assumes that res->retry_mutex is locked !!! */
+int retry_list_is_empty(struct sync_resources *res)
+{
+	return res->retry_tasks_head == NULL;
+}
---
+++ srptools.spec
@@ -0,0 +1,145 @@
+Name: srptools
+Version: 0.0.4
+Release: 17%{?dist}
+Summary: Tools for using the InfiniBand SRP protocol devices
+Group: System Environment/Base
+License: GPLv2 or BSD
+Url: http://www.openfabrics.org/
+
+Source0: http://www.openfabrics.org/downloads/%{name}/%{name}-%{version}-0.1.gce1f64c.tar.gz
+Source1: srptools.init
+Source2: srptools.service
+BuildRequires: libibumad-devel, libibverbs-devel > 1.1.3, systemd
+Requires(pre): systemd
+Requires(preun): systemd
+Requires(postun): systemd
+ExcludeArch: s390 s390x
+Obsoletes: openib-srptools <= 0.0.6
+
+%description
+In conjunction with the kernel ib_srp driver, srptools allows you to
+discover and use SCSI devices via the SCSI RDMA Protocol over InfiniBand.
+
+%package sysv
+Summary: Back-compatible SysV Init script for srptools
+Group: System Environment/Base
+Requires(pre): chkconfig
+Requires(preun): chkconfig /sbin/service
+
+%description sysv
+Backward compatible SysV Init script for srptools package
+
+%prep
+%setup -q
+
+%build
+%configure
+make CFLAGS="$CFLAGS -fno-strict-aliasing" %{?_smp_mflags}
+
+%install
+make DESTDIR=%{buildroot} install
+install -p -m 755 -D %{SOURCE1} %{buildroot}%{_initrddir}/srpd
+install -p -m 755 -D %{SOURCE2} %{buildroot}%{_unitdir}/srpd.service
+
+%post
+%systemd_post srpd.service
+
+%preun
+%systemd_preun srpd.service
+
+%postun
+%systemd_postun
+
+%post sysv
+if [ $1 = 1 ]; then
+    /sbin/chkconfig --add srpd
+fi
+
+%preun sysv
+if [ $1 = 0 ]; then
+    /sbin/service srpd stop
+    /sbin/chkconfig --del srpd
+fi
+
+%files
+%defattr(-,root,root)
+%config(noreplace) %{_sysconfdir}/srp_daemon.conf
+%{_unitdir}/srpd.service
+%{_sbindir}/ibsrpdm
+%{_sbindir}/srp_daemon
+%{_sbindir}/srp_daemon.sh
+%{_sbindir}/run_srp_daemon
+%{_mandir}/man1/ibsrpdm.1*
+%{_mandir}/man1/srp_daemon.1*
+%doc README NEWS ChangeLog COPYING
+
+%files sysv
+%defattr(-,root,root)
+%{_initrddir}/srpd
+
+%changelog
+* Fri Feb 15 2013 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 0.0.4-17
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_19_Mass_Rebuild
+
+* Wed Dec 05 2012 Doug Ledford <dledford@redhat.com> - 0.0.4-16
+- Add native systemd support
+- Resolves: bz884274
+
+* Sat Jul 21 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 0.0.4-15
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_18_Mass_Rebuild
+
+* Wed Jul  4 2012 Peter Robinson <pbrobinson@fedoraproject.org> - 0.0.4-14
+- Enable building on ARM, modernise spec
+
+* Tue Apr 24 2012 Doug Ledford <dledford@redhat.com> - 0.0.4-13
+- Further tweaks to LSB init script headers
+- Add a %%post and %%preun scriptlets to add/remove the init script
+
+* Tue Feb 28 2012 Doug Ledford <dledford@redhat.com> - 0.0.4-12
+- Fix minor issue in init script LSB headers
+
+* Fri Jan 06 2012 Doug Ledford <dledford@redhat.com> - 0.0.4-11
+- Initial import into Fedora
+
+* Wed Jul 27 2011 Doug Ledford <dledford@redhat.com> - 0.0.4-10.el6
+- Fix build on i686 arch
+- Resolves: bz724900
+
+* Thu Feb 03 2011 Doug Ledford <dledford@redhat.com> - 0.0.4-9.el6
+- Add srpd init script
+- Resolves: bz591169, bz658633, bz658674
+
+* Mon Jan 25 2010 Doug Ledford <dledford@redhat.com> - 0.0.4-8.el6
+- Update to latest upstream version so we can have an actual URL that works
+- Fix ups for pkgwrangler import and also fix bug reported against rhel5
+  version of package (552915)
+- Related: bz543948
+
+* Tue Dec 22 2009 Doug Ledford <dledford@redhat.com> - 0.0.4-7.el5
+- Bump and rebuild against new libibumad
+- Related: bz518218
+
+* Mon Jun 22 2009 Doug Ledford <dledford@redhat.com> - 0.0.4-6.el5
+- Rebuild against libibverbs that isn't missing the proper ppc wmb() macro
+- Related: bz506258
+
+* Sun Jun 21 2009 Doug Ledford <dledford@redhat.com> - 0.0.4-5.el5
+- Build against non-XRC libibverbs
+- Update to ofed 1.4.1 final bits
+- Related: bz506258, bz506097
+
+* Fri Apr 24 2009 Doug Ledford <dledford@redhat.com> - 0.0.4-4.el5
+- Add -fno-strict-aliasing to CFLAGS
+
+* Sat Apr 18 2009 Doug Ledford <dledford@redhat.com> - 0.0.4-3.el5
+- Bump and rebuild against updated libibverbs and libibumad
+- Related: bz459652
+
+* Tue Apr 01 2008 Doug Ledford <dledford@redhat.com> - 0.0.4-2
+- Update to OFED 1.3 final bits
+- Related: bz428197
+
+* Tue Jan 29 2008 Doug Ledford <dledford@redhat.com> - 0.0.4-1
+- Import upstream srptools and obsolete old openib-srptools package
+- Related: bz428197
+
